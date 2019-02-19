7fef3b66801f6f52b943942a18429fec025bde03
Damien <44206292+Debilux@users.noreply.github.com>
2019-02-19 22:00:10 +0100

    :arrow_up: Features et bientot JS

---
+++ scripts/features.js
@@ -0,0 +1,25 @@
+const technologie = document.querySelector('#technologie')
+const technologieButton = document.querySelector('#technologie_button')
+const sources = document.querySelector('#sources')
+const sourcesButton = document.querySelector('#sources_button')
+const formats = document.querySelector('#formats')
+const formatsButton = document.querySelector('#formats_button')
+const accessibilite = document.querySelector('#accessibilite')
+const accessibiliteButton = document.querySelector('#accessibilite_button')
+const configuration = document.querySelector('#configuration')
+const configurationButton = document.querySelector('#configuration_button')
+const alimentation = document.querySelector('#alimentation')
+const alimentationButton = document.querySelector('#alimentation_button')
+
+
+
+/*
+function masquer_div(id)
+{
+  if (.style.display == 'none') {
+       .style.display = 'block';
+  }
+  else {
+       .style.display = 'none';
+  }
+}*/
