415b023fd7227e0b55772429224af272457d4e96
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-02-21 11:23:20 +0100

    :lipstick: add slider on the buy.html and fix position

--- scripts/buy.js
+++ scripts/buy.js
@@ -1,4 +1,5 @@
-console.log('hey')
+//buy button
+
 const $appleCare = document.querySelector('.infos-care')
 const $price = document.querySelector('.infos-price-full')
 let isAdded = true
@@ -14,3 +15,44 @@ $appleCare.addEventListener('click', () => {
         isAdded = true
     }
 })
+
+//slider
+
+const pictures 		  = document.querySelectorAll(".sliderContainer img")
+const sliderContainer = document.querySelector(".sliderContainer")
+const dots			  = document.querySelectorAll("ul.dots li")
+
+let time = 3000,
+	step = -420,
+	currentPicture = 0,
+	slide
+
+function slideInterval(){
+	slide = setInterval(() =>{
+		currentPicture = (currentPicture + 1) % pictures.length
+		display(currentPicture)
+	}, time)
+}
+
+slideInterval()
+
+sliderContainer.addEventListener("mouseover", () => {
+	clearInterval(slide)
+})
+sliderContainer.addEventListener("mouseleave", () => {
+	slideInterval()
+})
+
+function display (currentPos){
+	sliderContainer.style.left = currentPos * step + "px"
+  document.querySelector('a.current').classList.remove('current')
+	dots[currentPos].querySelector('a').classList.add('current')
+}
+for(let i = 0; i < dots.length; i++){
+	dots[i].querySelector("a").addEventListener("click", (e) => {
+		e.preventDefault()
+		currentPicture = i
+		display(currentPicture)
+	})
+
+}
